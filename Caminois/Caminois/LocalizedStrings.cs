931f5feba1eb67b605c4932d220c34bdbc2caab7
qtstudio <qtstudio@outlook.com>
2015-02-03 20:27:50 +0700

    First project

---
+++ Caminois/Caminois/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Caminois.Resources;
+
+namespace Caminois
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
